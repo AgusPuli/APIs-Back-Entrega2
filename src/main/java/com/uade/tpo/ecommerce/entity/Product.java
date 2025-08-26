0150826afe5466b88440358adb1be4d95e31cce9
user-valen <valentinigarzabal11@gmail.com>
2025-08-26 19:03:07 -0300

    initial commit

--- src/main/java/com/uade/tpo/ecommerce/entity/Product.java
+++ src/main/java/com/uade/tpo/ecommerce/entity/Product.java
@@ -1,4 +1,35 @@
 package com.uade.tpo.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 }
--- src/main/java/com/uade/tpo/ecommerce/entity/User.java
+++ src/main/java/com/uade/tpo/ecommerce/entity/User.java
@@ -1,12 +1,22 @@
 package com.uade.tpo.ecommerce.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+//@Table(name = "users", uniqueConstraints = {
+//        @UniqueConstraint(name = "uk_users_email", columnNames = "email")
+//})
+
 public class User {
 
     @Id
@@ -14,9 +24,17 @@ public class User {
     private Long id;
 
     @Column
+    private String firstName;
 
     @Column
+    private String lastName;
+
+    @Column(nullable = false, length = 150)
     private String email;
+
+    @Column(nullable = false, length = 225)
+    private String password;
+
 
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
